495f5fa51799c5d2f1f4f619799336992c40d224
AdrianLG2002 <adrianisraellg2002@gmail.com>
2025-08-29 16:19:00 -0500

    Subida inicial del proyecto Node.js para PDF

--- server.js
+++ server.js
@@ -4,23 +4,22 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = 3000;
 
 // 1. Servir archivos estáticos desde /public
 app.use(express.static(path.join(__dirname, 'public')));
 
 // 2. Ruta especial que imita la URL larga con ?nuSoli=xxxx
 app.get('/portalUL/tdta/servlets/ComandoMostrarDocumentoTDTA', (req, res) => {
-  const nuSoli = req.query.nuSoli; // capturamos ?nuSoli=174453
+  const nuSoli = req.query.nuSoli; // Capturamos ?nuSoli=174453
 
   if (!nuSoli) {
     return res.status(400).send('Falta el parámetro nuSoli');
   }
 
-  // ruta del PDF según el parámetro
+  // Ruta del PDF según el parámetro
   const pdfPath = path.join(__dirname, 'public', 'pdfs', `${nuSoli}.pdf`);
 
-  // validamos que exista el archivo
+  // Validamos que exista el archivo
   if (fs.existsSync(pdfPath)) {
     res.sendFile(pdfPath);
   } else {
@@ -28,7 +27,6 @@ app.get('/portalUL/tdta/servlets/ComandoMostrarDocumentoTDTA', (req, res) => {
   }
 });
 
-// 3. Levantar el servidor
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+// Exportar la app (Vercel se encarga de ejecutarla)
+module.exports = app;
+
